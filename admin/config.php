6940f9287201c74198accd38959da814725ad84c
ENZO-PC\ENZO <anthony.enzo.chang@gmail.com>
2025-01-15 17:04:40 +0800

    After Installation

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tdirect.dev2.local/admin/');
+define('HTTP_CATALOG', 'http://tdirect.dev2.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tdirect.dev2.local/admin/');
+define('HTTPS_CATALOG', 'http://tdirect.dev2.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/admin/');
+define('DIR_SYSTEM', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/system/');
+define('DIR_IMAGE', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dev2');
+define('DB_PASSWORD', '3KSJLtWmUZKXZir');
+define('DB_DATABASE', 'tdirect_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tdirect.dev2.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tdirect.dev2.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/catalog/');
+define('DIR_SYSTEM', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/system/');
+define('DIR_IMAGE', '/home/dev2/domains/tdirect.dev2.local/public_html/tenderdirect/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dev2');
+define('DB_PASSWORD', '3KSJLtWmUZKXZir');
+define('DB_DATABASE', 'tdirect_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
